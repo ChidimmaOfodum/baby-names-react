61c4d8e0ef520905c5d83a8f22f9ce46e7825da9
Chidimma <chidimmamofodum@gmail.com>
2022-11-29 13:21:59 +0000

    combined filter and search functionalities

--- baby-names-react/src/App.js
+++ baby-names-react/src/App.js
@@ -5,23 +5,50 @@ import React, { useState } from "react";
 import Button from "./Button";
 
 function App() {
-  const [gender, setGender] = useState(babyData)
- 
+  const [gender, setGender] = useState(babyData);
+  let [data, setData] = useState()
 
-   function handleGender(event) {
-    if(event.currentTarget.className === "boy") setGender(babyData.filter((name) => name.sex === "m"));
-    else if(event.currentTarget.className === "girl") setGender(babyData.filter((name) => name.sex === "f"));
-    else (setGender(babyData))
-   }
+  function handleGender(event) {
+    if (event.currentTarget.className === "boy") {
+      setGender(babyData.filter((name) => name.sex === "m"));
+     setData("Male");
+    } else if (event.currentTarget.className === "girl") {
+      setGender(babyData.filter((name) => name.sex === "f"));
+      setData("Female");
+    } else {
+      setGender(babyData)
+      setData("All")
+    }
+  }
 
- 
+  function handleInput(event) {
+    if (data === "Male") {
+      let Male = babyData.filter((name) => name.sex === "m");
+      let test = Male.filter((name) => name.name.includes(event.target.value))
+      setGender(test)
+    }
+
+     if (data === "Female") {
+       let Female = babyData.filter((name) => name.sex === "f");
+       let test = Female.filter((name) => name.name.includes(event.target.value));
+       setGender(test);
+     }
+
+      else {
+        let test = babyData.filter((name) =>
+          name.name.includes(event.target.value)
+        );
+        setGender(test);
+      }
+    
+  }
 
   return (
     <div className="App">
-      <input type="search" />
-      <Button onClick = {handleGender}/>
-      <Button color="girl" onClick = {handleGender}/>
-      <Button color="boy" onClick = {handleGender} />
+      <input type="search"  onChange={handleInput}/>
+      <Button onClick={handleGender} />
+      <Button color="girl" onClick={handleGender} />
+      <Button color="boy" onClick={handleGender} />
       <BabyNames namesDisplay={gender} />
     </div>
   );
